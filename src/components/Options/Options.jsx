227c3b3c2cd528154ac53f0bd8035cc229e2e63e
Vladyslav Aka Tsaigetsu <callmepempai@gmail.com>
2024-07-06 14:05:21 +0200

    fix button

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,12 +1,12 @@
 
 
-const Options = ({onGoodClick, onNeutralClick, onBadClick, onResetClick}) => {
+const Options = ({onGoodClick, onNeutralClick, onBadClick, onResetClick, hasFeedback}) => {
   return (
     <div>
       <button onClick={onGoodClick}>Good</button>
       <button onClick={onNeutralClick}>Neutral</button>
       <button onClick={onBadClick}>Bad</button>
-      {hasFeedback && <button onClick={onResetClick}></button>}
+      {hasFeedback && <button onClick={onResetClick}>Reset</button>}
     </div>
   );
 };
